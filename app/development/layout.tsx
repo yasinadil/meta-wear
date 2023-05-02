06243020f5a4ffb90a024ff1a36d85c38bcd760d
yasinadil <adilyasin205@gmail.com>
2023-05-02 13:12:05 +0500

    revisions1

--- app/development/layout.tsx
+++ app/development/layout.tsx
@@ -3,12 +3,6 @@ import Navbar from "@/components/Navbar/Navbar";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import "../globals.css";
 
-export const metadata = {
-  title: "Development | Stealth Meta Wear",
-  description:
-    "Stealth Meta Wear is an innovative project that merges the realms of digital fashion and NFT (Non-Fungible Token) art. It aims to create a unique and cutting-edge collection of digital fashion items that are tokenized as NFTs on the blockchain.",
-};
-
 export default function RootLayout({
   children,
 }: {
